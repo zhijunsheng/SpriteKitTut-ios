683ec1e59441ebe906a0690ff54b04f16c9bb2c8
Golden Thumb <zhijunsheng@mbp2020.local>
2021-06-17 20:58:33 -0400

    created an empty SpriteKit project

---
+++ SpriteKitTut/SpriteKitTut/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  SpriteKitTut
+//
+//  Created by Golden Thumb on 2021-06-17.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+}
---
+++ SpriteKitTut/SpriteKitTut/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  SpriteKitTut
+//
+//  Created by Golden Thumb on 2021-06-17.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+
+        }
+    }
+
+}
