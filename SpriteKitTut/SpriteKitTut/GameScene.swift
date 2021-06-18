2de6d44a9fe94252c56e2106c692c1f1dfc48fc5
Golden Thumb <zhijunsheng@mbp2020.local>
2021-06-17 23:03:51 -0400

    created a sprite from a png image

--- SpriteKitTut/SpriteKitTut/GameScene.swift
+++ SpriteKitTut/SpriteKitTut/GameScene.swift
@@ -11,13 +11,16 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        let sprite = SKSpriteNode(color: UIColor.yellow, size: CGSize(width: 100, height: 200))
+        backgroundColor = UIColor.white
+        
+        let sprite = SKSpriteNode(imageNamed: "ant0")
         sprite.position = CGPoint(x: 300, y: 300)
         addChild(sprite)
         
         let action1 = SKAction.move(to: CGPoint(x: 600, y: 800), duration: 3)
         let action2 = SKAction.scale(by: 2, duration: 3)
         let action3 = SKAction.rotate(byAngle: 0.5 * CGFloat.pi, duration: 3)
+//        let action4 = SKAction.move(to: CGPoint(x: 0, y: 800), duration: 1)
         let actionGroup = SKAction.sequence([action1, action2, action3])
         
         sprite.run(actionGroup)
